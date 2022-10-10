6d912a9f4bb7f57f3ecc8da32e544ae64bebe819
Mykhail92 <movemens1992@gmail.com>
2022-10-10 18:12:05 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const btnStart = document.querySelector ('button[data-start]')
+const btnStop = document.querySelector('button[data-stop]')
+btnStart.addEventListener('click', onClick)
+btnStop.addEventListener('click',onStop)
+btnStop.disabled= true
 
+function onClick() {
+    btnStart.disabled = true;
+    btnStop.disabled = false
+   timerId= setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+}
+
+function onStop() {
+    btnStart.disabled = false;
+    btnStop.disabled = true
+    // document.body.style.backgroundColor = 'white'
+    clearInterval(timerId)
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+console.log('ddddddd');
