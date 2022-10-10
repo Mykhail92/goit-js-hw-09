cc768f82a164cea9f3fa232f2c32806d77c4e695
Mykhail92 <104631506+Mykhail92@users.noreply.github.com>
2022-10-10 15:12:29 +0000

    Deploying to gh-pages from @ Mykhail92/goit-js-hw-09@6d912a9f4bb7f57f3ecc8da32e544ae64bebe819 🚀

---
+++ 01-color-switcher.11e93233.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,d.disabled=!1,timerId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),d.addEventListener("click",(function(){t.disabled=!1,d.disabled=!0,clearInterval(timerId)})),d.disabled=!0,console.log("ddddddd")}();
+//# sourceMappingURL=01-color-switcher.11e93233.js.map
