e418818caa84660c905aec722497244021687008
Mykhail92 <movemens1992@gmail.com>
2022-10-12 12:40:01 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,7 +2,8 @@ const btnStart = document.querySelector ('button[data-start]')
 const btnStop = document.querySelector('button[data-stop]')
 btnStart.addEventListener('click', onClick)
 btnStop.addEventListener('click',onStop)
-btnStop.disabled= true
+btnStop.disabled = true
+let timerId =null
 
 function onClick() {
     btnStart.disabled = true;
@@ -23,4 +24,3 @@ function onStop() {
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-console.log('ddddddd');
